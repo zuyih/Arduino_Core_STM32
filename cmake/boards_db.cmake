37885bb5610b628b91b0efc06192076cacb93b08
Zuyi Hu <hzy068808@gmail.com>
2025-12-08 16:14:52 +0800

    add GENERIC_U575VGTX support

--- cmake/boards_db.cmake
+++ cmake/boards_db.cmake
@@ -101750,6 +101750,88 @@ target_compile_options(GENERIC_U575CIUX_xusb_HSFS INTERFACE
   "SHELL:-DUSE_USB_HS -DUSE_USB_HS_IN_FS"
 )
 
+# GENERIC_U575VGTX
+# -----------------------------------------------------------------------------
+
+set(GENERIC_U575VGTX_VARIANT_PATH "${CMAKE_CURRENT_LIST_DIR}/../variants/STM32U5xx/U575V(G-I)T_U585VIT")
+set(GENERIC_U575VGTX_MAXSIZE 1048576)
+set(GENERIC_U575VGTX_MAXDATASIZE 786432)
+set(GENERIC_U575VGTX_MCU cortex-m33)
+set(GENERIC_U575VGTX_FPCONF "-")
+add_library(GENERIC_U575VGTX INTERFACE)
+target_compile_options(GENERIC_U575VGTX INTERFACE
+  "SHELL:-DSTM32U575xx"
+  "SHELL:"
+  "SHELL:"
+  "SHELL:-mfpu=fpv4-sp-d16 -mfloat-abi=hard"
+  -mcpu=${GENERIC_U575VGTX_MCU}
+)
+target_compile_definitions(GENERIC_U575VGTX INTERFACE
+  "STM32U5xx"
+	"ARDUINO_GENERIC_U575VGTX"
+	"BOARD_NAME=\"GENERIC_U575VGTX\""
+	"BOARD_ID=GENERIC_U575VGTX"
+	"VARIANT_H=\"variant_generic.h\""
+)
+target_include_directories(GENERIC_U575VGTX INTERFACE
+  ${CMAKE_CURRENT_LIST_DIR}/../system/STM32U5xx
+  ${CMAKE_CURRENT_LIST_DIR}/../system/Drivers/STM32U5xx_HAL_Driver/Inc
+  ${CMAKE_CURRENT_LIST_DIR}/../system/Drivers/STM32U5xx_HAL_Driver/Src
+  ${CMAKE_CURRENT_LIST_DIR}/../system/Drivers/CMSIS/Device/ST/STM32U5xx/Include/
+  ${CMAKE_CURRENT_LIST_DIR}/../system/Drivers/CMSIS/Device/ST/STM32U5xx/Source/Templates/gcc/
+  ${GENERIC_U575VGTX_VARIANT_PATH}
+)
+
+target_link_options(GENERIC_U575VGTX INTERFACE
+  "LINKER:--default-script=${GENERIC_U575VGTX_VARIANT_PATH}/ldscript.ld"
+  "LINKER:--defsym=LD_FLASH_OFFSET=0x0"
+	"LINKER:--defsym=LD_MAX_SIZE=2097152"
+	"LINKER:--defsym=LD_MAX_DATA_SIZE=786432"
+  "SHELL:-mfpu=fpv4-sp-d16 -mfloat-abi=hard"
+  -mcpu=${GENERIC_U575VGTX_MCU}
+)
+
+add_library(GENERIC_U575VGTX_serial_disabled INTERFACE)
+target_compile_options(GENERIC_U575VGTX_serial_disabled INTERFACE
+  "SHELL:"
+)
+add_library(GENERIC_U575VGTX_serial_generic INTERFACE)
+target_compile_options(GENERIC_U575VGTX_serial_generic INTERFACE
+  "SHELL:-DHAL_UART_MODULE_ENABLED"
+)
+add_library(GENERIC_U575VGTX_serial_none INTERFACE)
+target_compile_options(GENERIC_U575VGTX_serial_none INTERFACE
+  "SHELL:-DHAL_UART_MODULE_ENABLED -DHWSERIAL_NONE"
+)
+add_library(GENERIC_U575VGTX_usb_CDC INTERFACE)
+target_compile_options(GENERIC_U575VGTX_usb_CDC INTERFACE
+  "SHELL:-DUSBCON -DUSBD_VID=0x0483 -DUSBD_PID=0x5740 -DHAL_PCD_MODULE_ENABLED -DUSBD_USE_CDC -DDISABLE_GENERIC_SERIALUSB"
+)
+add_library(GENERIC_U575VGTX_usb_CDCgen INTERFACE)
+target_compile_options(GENERIC_U575VGTX_usb_CDCgen INTERFACE
+  "SHELL:-DUSBCON -DUSBD_VID=0x0483 -DUSBD_PID=0x5740 -DHAL_PCD_MODULE_ENABLED -DUSBD_USE_CDC"
+)
+add_library(GENERIC_U575VGTX_usb_HID INTERFACE)
+target_compile_options(GENERIC_U575VGTX_usb_HID INTERFACE
+  "SHELL:-DUSBCON -DUSBD_VID=0x0483 -DUSBD_PID=0x5740 -DHAL_PCD_MODULE_ENABLED -DUSBD_USE_HID_COMPOSITE"
+)
+add_library(GENERIC_U575VGTX_usb_none INTERFACE)
+target_compile_options(GENERIC_U575VGTX_usb_none INTERFACE
+  "SHELL:"
+)
+add_library(GENERIC_U575VGTX_xusb_FS INTERFACE)
+target_compile_options(GENERIC_U575VGTX_xusb_FS INTERFACE
+  "SHELL:"
+)
+add_library(GENERIC_U575VGTX_xusb_HS INTERFACE)
+target_compile_options(GENERIC_U575VGTX_xusb_HS INTERFACE
+  "SHELL:-DUSE_USB_HS"
+)
+add_library(GENERIC_U575VGTX_xusb_HSFS INTERFACE)
+target_compile_options(GENERIC_U575VGTX_xusb_HSFS INTERFACE
+  "SHELL:-DUSE_USB_HS -DUSE_USB_HS_IN_FS"
+)
+
 # GENERIC_U575ZGTXQ
 # -----------------------------------------------------------------------------
 
